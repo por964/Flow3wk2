eea76ded42256ae4fa9eb4e645ce6d7b5b9b97c6
Claes <claesvonh@gmail.com>
2020-11-08 22:39:03 +0100

    open files

--- jokeFetcher-momondo-demo/src/main/java/rest/JokeResource.java
+++ jokeFetcher-momondo-demo/src/main/java/rest/JokeResource.java
@@ -9,11 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 
-/**
- * REST Web Service
- *
- * @author lam
- */
 @Path("jokes")
 public class JokeResource {
 
